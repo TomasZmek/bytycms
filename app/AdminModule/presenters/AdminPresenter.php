bb4591c76595654fe032f04d774bb66517086875
Tomáš Zmek <perteus@gmail.com>
2013-05-08 15:37:21 +0200

    Admin sekce
    Dokončená admin sekce z pohledu přihlašování. :-)

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -1,9 +1,53 @@
 <?php
-
+use Nette\Application\UI\Form;
+use Nette\Security\AuthenticationException;
 /**
  * Homepage presenter.
  */
 class AdminPresenter extends BasePresenter
 {
+	  /** @persistent */
+    public $backlink;
+
+
+    /**
+     * Login form factory
+     * @return Nette\Application\UI\Form
+     */
+	protected function createComponentLoginForm()
+    {
+        $form = new Form;
+        $form->addText('name', 'Name:')
+            ->addRule(Form::FILLED, 'Enter login');
+        $form->addPassword('password', 'Password:')
+            ->addRule(Form::FILLED, 'Enter password');
+        $form->addSubmit('send', 'Log in');
+
+        $form->onSuccess[] = $this->processLoginForm;
+        return $form;
+    }
+
+
+    /**
+     * Process login form and login user
+     * @param Nette\Application\UI\Form
+     */
+    public function processLoginForm(Form $form)
+    {
+        $values = $form->getValues(TRUE);
+        try {
+            $this->user->login($values['name'], $values['password']);
+            $this->restoreRequest($this->backlink);
+            $this->redirect('Admin:default');
+
+        } catch (AuthenticationException $e) {
+            $form->addError($e->getMessage());
+        }
+    }
 	
+	public function handleLogOut()
+ 	{
+    	$this->getUser()->logout();
+  		$this->redirect('Admin:auth');
+ 	}
 }
--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -1,35 +1,30 @@
 <?php
 
-use Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route,
-	Nette\Application\Routers\SimpleRouter;
-
+use Nette\Application\Routers\RouteList, Nette\Application\Routers\Route, Nette\Application\Routers\SimpleRouter;
 
 /**
  * Router factory.
  */
-class RouterFactory
-{
+class RouterFactory {
 
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public function createRouter()
-	{
+	public function createRouter() {
 		$router = new RouteList();
-		$router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-		
-		$router[] = new Route('admin/<presenter>/<action>[/<id>]', array(
-				'module' => 'Admin',
-				'presenter' => 'Base',
-				'action' => 'default',
-			));
-			/*$router[] = new Route('byty/<presenter>/<action>[/<id>]', array(
-				'module' => 'Byty',
-				'presenter' => 'Homepage',
-				'action' => 'default',
-			)); */
+		 $router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
+		 $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
+		 $router[] = new Route('admin/<presenter>/<action>[/<id>]', array(
+		 'module' => 'Admin',
+		 'presenter' => 'Base',
+		 'action' => 'default',
+		 ));
+		 /*$router[] = new Route('byty/<presenter>/<action>[/<id>]', array(
+		 'module' => 'Byty',
+		 'presenter' => 'Homepage',
+		 'action' => 'default',
+		 )); */
 		return $router;
 	}
 
